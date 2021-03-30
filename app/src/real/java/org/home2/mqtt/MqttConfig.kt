a721459c6906132e8674dc9241d7cb6acf5eb861
Maxim Tkachenko <mtkachenko@ebay.com>
2021-03-30 21:14:30 +0200

    Project compiles on GH.

---
+++ app/src/real/java/org/home2/mqtt/MqttConfig.kt
@@ -0,0 +1,8 @@
+package org.home2.mqtt
+
+/**
+ * Created by mtkachenko on 18/11/17.
+ */
+const val MQTT_SERVER_URL = "ssl://a.b.c"
+const val MQTT_USERNAME = "abc"
+const val MQTT_PASSWORD = "abc"
