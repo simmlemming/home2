e6fcd876f3aae81fabc3345e6ecba3497988f837
Maxim Tkachenko <mtkachenko@ebay.com>
2021-03-30 21:15:07 +0200

    Revert "Project compiles on GH."
    This reverts commit a721459c6906132e8674dc9241d7cb6acf5eb861.

--- app/src/real/java/org/home2/mqtt/MqttConfig.kt
+++
@@ -1,8 +0,0 @@
-package org.home2.mqtt
-
-/**
- * Created by mtkachenko on 18/11/17.
- */
-const val MQTT_SERVER_URL = "ssl://a.b.c"
-const val MQTT_USERNAME = "abc"
-const val MQTT_PASSWORD = "abc"
